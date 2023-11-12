98188db7929e62f9bb40b3f229603912bf808626
pcardona79 <paolitacg@gmail.com>
2023-11-11 21:45:43 -0500

    se quita la url

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL','https://apis.diplomadoisucomfamiliar.net'),
+            //'url' => env('DATABASE_URL','https://apis.diplomadoisucomfamiliar.net'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'DBlaravel '),
