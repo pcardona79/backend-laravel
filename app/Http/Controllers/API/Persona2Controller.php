47d0714bfa84aa09621120f928775b3df5df73fb
pcardona79 <paolitacg@gmail.com>
2023-11-23 18:29:16 -0500

    conexion

--- app/Http/Controllers/API/Persona2Controller.php
+++ app/Http/Controllers/API/Persona2Controller.php
@@ -39,9 +39,58 @@ class Persona2Controller extends Controller
     
         } catch (\Throwable $th) {
             //throw $th;
-            return response()->json(['error' => $th ->getMessage()],500);
+            return response()->json(['error' => $th ->getMessage(),500]);
         }
 
     }
+  
+
+    public function getById($id)
+
+    {
+            try {              
+                   $data=Persona::find($id);
+                   return response()->json($data,200);
+
+            } catch (\Throwable $th) {
+                return response()->json(['error' => $th ->getMessage(),500]);
+            }
+
+    }
+
+
+   public  function update(Request $request ,$id)
+   {
+
+        try {
+            $data['nombre']=$request['nombre'];
+            $data['direccion']=$request['direccion'];
+            $data['telefono']=$request['telefono'];
+
+            Persona::find($id)->update($data); 
+            $res=Persona::find($id);
+            return response()->json($res,200);
+
+        } catch (\Throwable $th) {
+            return response()->json(['error' => $th ->getMessage(),500]);
+        }
+
+   }
+
+
+   public function delete($id)
+   {
+
+
+       try {
+             $res=Persona::find($id)->delete();
+             return response()->json(['Registro eliminado' => $res ] ,200);
+       } catch (\Throwable $th) {
+        return response()->json(['error' => $th ->getMessage(),500]);
+       }
+
+   }
+
+
 
 }
--- routes/api.php
+++ routes/api.php
@@ -22,17 +22,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::prefix('v1/personas')->group(function () {
-    // Route::get('/', function () {
-    //     return view('welcome');
-    // });
+ 
     Route::get('/',[ PersonaController::class, 'get']);
     Route::post('/',[ PersonaController::class, 'create']);
+    Route::get('/{id}',[ PersonaController::class, 'getById']);
+    Route::put('/{id}',[ PersonaController::class, 'update']);
+    Route::delete('/{id}',[ PersonaController::class, 'delete']);
 
-//   
-//     Route::get('/{id}',[ PersonaController::class, 'getById']);
-//     Route::put('/{id}',[ PersonaController::class, 'update']);
-//     Route::delete('/{id}',[ PersonaController::class, 'delete']);
-//
  });
 
  
